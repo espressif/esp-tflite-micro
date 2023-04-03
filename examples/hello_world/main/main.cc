80ac37d4cb93b100032cc8e5c46562dfcc5214d0
Vikram <vikram.dattu@espressif.com>
2023-04-03 18:44:25 +0530

    hello_world example: File one inference every 500ms
    Signed-off-by: Vikram <vikram.dattu@espressif.com>

--- examples/hello_world/main/main.cc
+++ examples/hello_world/main/main.cc
@@ -13,11 +13,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include "main_functions.h"
 
 extern "C" void app_main(void) {
   setup();
   while (true) {
     loop();
+
+    // trigger one inference every 500ms
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
